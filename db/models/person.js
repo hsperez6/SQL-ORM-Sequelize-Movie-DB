99d80feb6f369a349a38e5fd59d0801af70e072d
hsperez6 <hsperez@gmail.com>
2023-10-13 14:35:07 -0700

    Review 10/13/23

--- db/models/person.js
+++ db/models/person.js
@@ -1,45 +1,47 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = (sequelize) => {
   class Person extends Sequelize.Model {}
   Person.init(
     // Attributes Object
     {
-    id: {
-      type: Sequelize.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    firstName: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for "firstName"', 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "firstName"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "firstName"',
+          },
         },
-        notEmpty: {
-          msg: 'Please provide a value for "firstName"',
+      },
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: {
+            msg: 'Please provide a value for "lastName"',
+          },
+          notEmpty: {
+            msg: 'Please provide a value for "lastName"',
+          },
         },
       },
     },
-    lastName: {
-      type: Sequelize.STRING,
-      allowNull: false,
-      validate: {
-        notNull: {
-          msg: 'Please provide a value for "lastName"',
-        },
-        notEmpty: {
-          msg: 'Please provide a value for "lastName"',
-        },
-      },
-    },
-  }, 
-  // Model Options Object
-  { 
-    timestamps: false, 
-    freezeTableName: true,
-    sequelize });
+    // Model Options Object
+    {
+      timestamps: false,
+      freezeTableName: true,
+      sequelize,
+    }
+  );
 
   return Person;
 };
